24c11669b156ac305a395589b100a49fe547a66a
Sten <stenwiersema@outlook.com>
2021-06-01 13:42:50 +0200

    js, css and html skeleton added

--- index.php
+++ index.php
@@ -2,9 +2,25 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/main.css">
 	<title>PF - Home</title>
 </head>
 <body>
-
+	<div class="Header">
+		<img src="img/menu.png">
+		<img src="img/logo.png">
+	</div>
+	<div class="bodyWrapper">
+		<h1 class="title">Sven Wiersema</h1>
+		<p class="paraOne">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
 </body>
+<script type="text/javascript" src="scripts/events.js" />
 </html>
