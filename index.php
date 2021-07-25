cabde5c0240acd703f50d568572e7593bb54f724
Svenwiersema <stenwiersema@outlook.com>
2021-07-25 23:44:21 +0200

    made index bosy with sass and bootstrap

--- index.php
+++ index.php
@@ -8,12 +8,24 @@ include "snippets/head.php";
 	include "snippets/header.php";
 	?>
 	
-	<div class="bodyWrapper">
-		<p class="h1" id="demo">Sven Wiersema</p>
-		<p class="paraOne">
-			Mijn naam is Sven Wiersema en ik ben 22. Welkom op mijn portfolio site. Ik studeer aan het REA college in Groningen, en ik doe de robotica opleiding. Ik studeer daar nu een jaar. Ik heb voor deze opleiding verschillende andere opleidingen gedaan van netwerk beheer tot game development, dus je kan wel zeggen dat ik voor vrij veel programmeer talen een basis heb. Ik heb kennis van HTML tot aan JAVA, en via het menu kan je een lijst bekijken van al mijn projecten die ik heb gemaakt in de tijd die ik op het REA college zit.
-		</p>
-		<a href="files/CurriculumVitae.pdf" class="link" target="_blank">Mijn CV</a>
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col col-12 text-center mt-5">
+				<p class="h1" id="demo">Sven Wiersema</p>
+			</div>
+			<div class="col col-12 col-md-6 px-5 px-md-10 px-lg-20 mt-5 text-center">
+				<p>
+					Mijn naam is Sven Wiersema en ik ben 22. Welkom op mijn portfolio site. Ik studeer aan het REA college in Groningen, en ik doe de robotica opleiding. Ik studeer daar nu een jaar. Ik heb voor deze opleiding verschillende andere opleidingen gedaan van netwerk beheer tot game development, dus je kan wel zeggen dat ik voor vrij veel programmeer talen een basis heb. Ik heb kennis van HTML tot aan JAVA, en via het menu kan je een lijst bekijken van al mijn projecten die ik heb gemaakt in de tijd die ik op het REA college zit.
+				</p>
+			</div>
+			<div class="col col-12 col-md-6">
+				<div class="text-center vertical-center">
+					<a class="btn btn-lg btn-outline-primary" href="files/CurriculumVitae.pdf" class="link" target="_blank">Mijn CV</a>
+				</div>
+			</div>
+		</div>
+
+		
 	</div>
 	
 	<?php
--- snippets/footer.php
+++ snippets/footer.php
@@ -1,6 +1,6 @@
-<div class="footer">
-		<p>Made by Sven Wiersema</p>
-		<p>&#169RoboSten 2021</p>
-	</div>
-	<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
-	<script type="module" src="scripts/main.js"></script>
+<div class="fixed-bottom">
+	<p>Made by Sven Wiersema</p>
+	<p>&#169RoboSten 2021</p>
+</div>
+<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script type="module" src="scripts/main.js"></script>
