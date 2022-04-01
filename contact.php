b87af075fc93192bfbe08a99ae983e72cefa29e7
Sten <stenwiersema@outlook.com>
2022-04-01 16:14:46 +0200

    added contact page

--- contact.php
+++ contact.php
@@ -14,21 +14,17 @@ include "snippets/head.php";
 		<form name="contactform" action="php/email.php" method="post">
 			<div class="row">
 				<div class="col col-12 col-md-6">
-					<label for="firstName" class="form-label">Voornaam</label><span><i <?php ($_POST["nameErr"] ? "class='bi bi-x wrong'" : "class='bi bi-check good'"); ?>></i></span>
-					<input type="text" class="form-control" id="firstName" name="firstname">
+					<label for="name" class="form-label">Naam</label>
+					<input type="text" class="form-control" id="name" name="name" placeholder="Sten" pattern=[A-Z\sa-z]{3,20} required>
 					
 				</div>
-				<div class="col col-12-col-md-6">
-					<label for="lastName" class="form-label">Achternaam</label><span><i <?php ($_POST["lNameErr"] ? "class='bi bi-x wrong'" : "class='bi bi-check good'"); ?> id="validate"></i></span>
-					<input type="text" class="form-control" id="lastName" name="lastname">
+				<div class="col col-12 col-md-6">
+					<label for="email" class="form-label">E-mail</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="sten@example.com" required>
 				</div>
 				<div class="col col-12">
-					<label for="email" class="form-label">E-mail</label><span><i class="bi bi-x wrong" id="validateEmail"></i></span>
-					<input type="email" class="form-control" id="email" name="email">
-				</div>
-				<div class="col col-12">
-					<label for="message" class="form-label">Bericht</label><span><i class="bi bi-x wrong" id="validate"></i></span>
-					<textarea class="form-control" id="message" name="message" rows="10"></textarea>
+					<label for="message" class="form-label">Bericht</label>
+					<textarea class="form-control" id="message" name="message" rows="10" required></textarea>
 				</div>
 				<div class="col col-12 mt-4 text-center">
 					<button type="submit" class="btn btn-lg btn-outline-primary">Verstuur</button>
--- php/email.php
+++ php/email.php
@@ -1,30 +1,44 @@
 <?php
 
-$url = "../contact.php";
+if($_POST) {
+    $name = "";
+    $email = "";
+    $msg = "";
+    $emailBody = "";
 
-$name = $_POST["firstname"];
-$lName = $_POST["lastname"];
-$email = $_POST["email"];
-$message = $_POST["message"];
+    if(isset($_POST["name"])) {
+        $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+        $emailBody = "<div>
+                            <label><b>Naam: </b></label>
+                            <p>".$name."</p>
+                        </div>";
+    }
+    if(isset($_POST["email"])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST["email"]);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $emailBody .= "<div>
+                            <label><b>E-mail: </b></label>
+                            <p>".$email."</p>
+                        </div>";
+    }
+    if(isset($_POST["message"])) {
+        $msg = htmlspecialchars($_POST["message"]);
+        $emailBody .= "<div>
+                            <label><b>Bericht: </b></label>
+                            <p>".$msg."</p>
+                        </div>";
+    }
 
-if($name == null || $name == "") {
-    $nameErr = true;
+    $headers  = "MIME-Version: 1.0" . "\r\n"
+    ."Content-type: text/html; charset=utf-8" . "\r\n"
+    ."From: " .$email. "\r\n";
+    if(mail("contact@robosten.com", "Contact From", $emailBody, $headers)) {
+        header("Location: ../contact.php");
+    } else {
+        echo("oops, something went wrong.");
+    }
 } else {
-    $nameErr = false;
-}
-if($lName == null || $lName == "") {
-    $lNameErr = true;
-} else {
-    $lNameErr = false;
-}
-
-
-
-if($nameErr || $lNameErr) {
-    $_POST["nameErr"] = $nameErr;
-    $_POST["lNameErr"]= $lNameErr;
-
-    header("Location: $url");
+    header("Location: ../contact.php");
 }
 
 ?>
