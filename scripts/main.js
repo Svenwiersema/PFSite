a855c3ae5bea7cf0d00fc37a81d75dd8803a8fc3
Sten <stenwiersema@outlook.com>
2022-04-22 22:27:45 +0200

    fixed the masonry and made images smaller

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,30 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/imagesloaded/imagesloaded.pkgd.min.js";
+import "../node_modules/masonry-layout/dist/masonry.pkgd.min.js";
+
+// $(document).ready(function(){
+//     $(".grid").masonry({
+//         itemSelector: ".grid-item"
+//     })
+// });
+
+$('#masonry-grid').imagesLoaded( function() {
+    $(".grid").masonry({
+        itemSelector: ".grid-item"
+    });
+});
+
+// var $grid = $('.grid').masonry({
+//     itemSelector: ".grid-item"
+//   });
+//   // layout Masonry after each image loads
+//   $grid.imagesLoaded().progress( function() {
+//     $grid.masonry('layout');
+//   });
+
+// var $container = $("masonry-grid");
+// $container.imagesLoaded(function() {
+//     $(".grid").masonry({
+//         itemSelector: ".grid-item"
+//     });
+// });
--- snippets/head.php
+++ snippets/head.php
@@ -2,7 +2,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="style/main.css">
-	<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/imagesloaded/imagesloaded.pkgd.min.js"></script>
+	<script src="node_modules/masonry-layout/dist/masonry.pkgd.min.js"></script>
 	<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="RoboS" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 	<link rel="icon" href="img/icon.ico">
 	<title>RoboSten - PF</title>
